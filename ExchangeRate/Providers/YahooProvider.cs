ea2c8091b1de9866b65702fb8e6d5ae270bceff2
simonray <simonray@hotmail.com>
2017-10-03 18:30:33 +0100

    Include license information

--- ExchangeRate/Providers/YahooProvider.cs
+++ ExchangeRate/Providers/YahooProvider.cs
@@ -1,4 +1,6 @@
-﻿
+﻿// Copyright ©2017 Simonray (http://github.com/simonray). All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using ExchangeRate.Model;
 using System.Net;
 
